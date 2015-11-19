24bc0793da5ecc4f438a5948f6bb77c8437f5a38
Edward-Shaw <yxiao@aliyun.com>
2015-11-19 22:26:30 +0800

    1.add tt-cleint.html; 2.rewrite tt2http-client.

--- tt-client/tt2http-client.js
+++ tt-client/tt2http-client.js
@@ -21,75 +21,55 @@
 *
 */
 
-var TeamTalk = {
-	address: "172.18.0.236",
-	port: 8000,
-	socket: null,
-	createNew: function(var ipAddress, var port){
-		var tt = {};
-		TeamTalk.address = ipAddress;
-		TeamTalk.port = port;
-
-		tt.connect = function(){
-			TeamTalk.socket	= new WebSocket("ws://" + TeamTalk.address + ":" + TeamTalk.port);
-			TeamTalk.onmessage = onMessageReceived;
-		};
-
-		tt.login = function(var userName, var password){
-			TeamTalk.socket.send(userName + password);
-		};
-
-		tt.onMessageReceived = function(evt){
-			return evt.data;
-		};
-
-		return tt;
-	}
+<<<<<<< HEAD
+//construct function for TTWebClient.
+function TTWebClient(ipv4, port){
+	this.ipv4 = ipv4;
+	this.port = port;
+	this.socket = new WebSocket("ws://" + ipv4 + ":" + port);
+	//this.socket.binaryType = "arraybuffer";
+	this.socket.onopen = function(evt){this._onopen(evt)};
+	this.socket.onclose = function(evt){this._onclose(evt)};
+	this.socket.onmessage = function(message){this._onmessage(message)};
+	this.socket.onerror = function(evt){this._onerror(evt)};
 }
 
-
-//connect to TT2HTTP server.
-var socket = new WebSocket("ws://172.18.0.236:8080/ws");
-
-//communicate with binary data.
-socket.binaryType = "arraybuffer";
-
-//login to TT
-//TODO:add login arguments
-function login(var userName, var password){
-	message = userName + password;
-	send(message);
+TTWebClient.prototype.login = function(userName, password){
+	this._sendMessage(userName + password);
 }
 
-//add message header before send
-function beforeSend(var message){
-	//TODO:add message header
-	return message;
-}
-
-function send(var message) {
-    if (socket.readyState == WebSocket.OPEN) {
-        socket.send(beforeSend(message));
+TTWebClient.prototype._sendMessage = function(message){
+	if (this.socket.readyState == WebSocket.OPEN) {
+        this.socket.send(this._beforeSend(message));
     } else {
     	alert("Network Error!");
     }
 }
 
-socket.onopen = function() {
-    log.value += "Connected\n";
-};
+TTWebClient.prototype._beforeSend = function(message){
+	return message;
+}
 
-socket.onclose = function() {
-    log.value += "Disconnected\n";
-};
+TTWebClient.prototype._onopen = function(evt){
+	console.log("Connected\n");
+}
+
+TTWebClient.prototype._onclose = function(evt){
+	console.log("Disconnected\n");
+}
 
 //TODO:判断message类型并调用相应的onXXXMessageReceived()方法进行处理
-socket.onmessage = function(evt) {
-    try {
+TTWebClient.prototype._onmessage = function(message){
+	try {
         // Decode the Message
-        var msg = Message.decode(evt.data);
-        log.value += "Received: "+msg.text+"\n";
+        console.log("Received data: " + message.data);
     } catch (err) {
-        log.value += "Error: "+err+"\n";
+        console.log("Error occured: " + err + "\n");
     }
-};
+}
+
+TTWebClient.prototype._onerror = function(evt){
+	console.log("Error occured: " + evt.data + "\n");
+}
+
+
