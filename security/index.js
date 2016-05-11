df2090bf08f988c982f7b258e221638a52fb7a46
Edward-Shaw <yxiao@aliyun.com>
2016-05-11 11:54:50 +0800

    modify security.

--- security/index.js
+++ security/index.js
@@ -17,19 +17,36 @@ module.exports = {
     plainChunks.push(decipher.update(crypted.toString('hex'), cipherEncoding, clearEncoding));
     plainChunks.push(decipher.final(clearEncoding));
     
-    ///console.log( plainChunks.join(' ###XXXX '));
+    var result = new Buffer(plainChunks.join(''));
     
-    return new Buffer(plainChunks.join(''));
+    var len = result.readInt32BE(result.length - 4);
+    ///console.log("DecryptMsg", result.length, len);
+    result.writeInt32BE(0, result.length - 4);
+    
+    return result;
   },
   
   EncryptMsg: function(plain/*: String*/) /*: Buffer*/{
     var cipher = crypto.createCipheriv(algorithm, keys, iv);
     cipher.setAutoPadding(false);
 
+    var data = new Buffer(plain, 'utf8');
+    var v = data.length % 16, blocks = Math.floor((data.length + 15) / 16);
+    if(v > 12 || v == 0){
+      blocks += 1;
+    }
+    var newData = new Buffer(blocks * 16);
+    newData.fill(0);
+    data.copy(newData);
+    newData.writeInt32BE(data.length, blocks * 16 - 4);
+
     var cipherChunks = [];
-    cipherChunks.push(cipher.update(plain, clearEncoding, cipherEncoding));
+    cipherChunks.push(cipher.update(newData, clearEncoding, cipherEncoding));
     cipherChunks.push(cipher.final(cipherEncoding));
     
-    return new Buffer(cipherChunks.join(''));
+    var result = new Buffer(cipherChunks.join(''), 'hex');
+    var base64 = result.toString('base64');
+
+    return new Buffer(base64);
   }
 }
